bda13515358e88516cf54ecb6fb7a0f21e71c8d4
Said Masih <saidmsajady@gmail.com>
2024-06-02 16:27:14 -0700

    Succesfully connected the API and tested it with the title of the movie

--- scripts/script.mjs
+++ scripts/script.mjs
@@ -1,6 +1,21 @@
 async function fetchData() {
-    const result = await fetch('http(s)://gateway.marvel.com/');
-    //const data = await result.json();
+    const url = 'https://movie-database-alternative.p.rapidapi.com/?s=Avengers%20Endgame&r=json&page=1';
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'd1ed2eb844mshb4c39ce12a01b36p11f486jsn03d60747374c',
+            'x-rapidapi-host': 'movie-database-alternative.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+        const movieTitle = data.Search[0].Title; 
+        document.getElementById('movieTitle').innerText += ` ` + movieTitle;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 fetchData();
